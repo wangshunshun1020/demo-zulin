96ed775d73a404f603a99a1d30e12fe83cafd503
wangshunshun1020 <110956499+wangshunshun1020@users.noreply.github.com>
2025-03-17 13:40:08 +0800

    first commit

---
+++ application/extra/addons.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'autoload' => false,
+    'hooks' => [
+        'app_init' => [
+            'xylease',
+        ],
+        'upgrade' => [
+            'xylease',
+        ],
+    ],
+    'route' => [],
+    'priority' => [],
+    'domain' => '',
+];
